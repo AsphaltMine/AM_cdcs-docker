e6733116f6545adda4a30d3a0abd654f2c5d8b52
MA042024 <mohammad.abbas@empa.ch>
2024-10-30 12:22:22 +0100

    AM related modifications
    AM related modifications

--- build/cdcs/scripts/start-django-uwsgi.sh
+++ build/cdcs/scripts/start-django-uwsgi.sh
@@ -11,5 +11,9 @@ uwsgi --chdir /srv/curator/ \
       --processes=${PROCESSES:-8} \
       --threads=${THREADS:-8} \
       --enable-threads \
-      --lazy-apps
+      --lazy-apps \
+      --max-requests=100 \
+      --max-requests-delta=10 \
+      --reload-on-rss=2100 \
+      --worker-reload-mercy=60
 echo "UWSGI started"
